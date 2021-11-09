6f6c775e95d110884b2b232cc16d9dc4e718eb54
Matthew Hefferon <matthefferon@gmail.com>
2021-11-08 20:00:32 -0700

    Line chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import LineExample from "./components/LineExample";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <LineExample />
     </div>
   );
 }
---
+++ src/components/LineExample.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import data from "../data/line.json";
+
+const LineExample = () => {
+  return (
+    <div style={{ width: "80%", height: 300 }}>
+      <ResponsiveContainer>
+        <LineChart
+          width={800}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default LineExample;
